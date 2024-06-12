1728b7f995e5c1875e3ef5dbb3c25acd20afead4
nsfoxer <muwuren@gmail.com>
2024-06-12 21:36:43 +0800

    修复fcitx5输入法问题

--- modules/kde.nix
+++ modules/kde.nix
@@ -61,10 +61,16 @@
       mplus-outline-fonts.githubRelease
       dina-font
       proggyfonts
+      source-sans # 无衬线字体，不含汉字。字族名叫 Source Sans 3 和 Source Sans Pro，以及带字重的变体，加上 Source Sans 3 VF
+      source-serif # 衬线字体，不含汉字。字族名叫 Source Code Pro，以及带字重的变体
+      source-han-sans # 思源黑体
+      source-han-serif # 思源宋体
     ];
 
     # use fonts specified by user rather than default ones
     enableDefaultPackages = true;
+    fontDir.enable = true;
+
 
     # user defined fonts
     # the reason there's Noto Color Emoji everywhere is to override DejaVu's
@@ -83,14 +89,28 @@
     lshw
   ];
 
+  # fcitx5 输入法设置
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-        rime-data
+        rime-data # 必须加上，否则rime不可用
         fcitx5-rime
         fcitx5-gtk
     ];
+    fcitx5.waylandFrontend = true;
   };
+  # 重新设置kde下的输入法环境变量
+  lib.mkForce = {
+    environment.variables = {
+      #GTK_IM_MODULE = "fcitx";
+      GTK_IM_MODULE = "wayland";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+      SDL_IM_MODULE = "fcitx";
+      GLFW_IM_MODULE = "ibus";
+    };
+  };
+
 
   hardware.bluetooth.enable = true;
 

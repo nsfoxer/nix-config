a41c4e37da75d356a61d8c6aeb360e326c52eb05
nsfoxer <muwuren@gmail.com>
2024-07-25 22:27:37 +0800

    系统更新;修复ssh config文件权限错误问题

--- home/common/default.nix
+++ home/common/default.nix
@@ -4,9 +4,12 @@
   ...
 }: {
   # ssh config
-  home.file.".ssh/config" = {
-    # source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/common/configs/ssh/config";
+  home.file.".ssh/config.link" = {
     source = ./configs/ssh/config;
+    onChange = ''
+      cat ~/.ssh/config.link > ~/.ssh/config
+      chmod 600 ~/.ssh/config
+    '';
   };
 
 }
